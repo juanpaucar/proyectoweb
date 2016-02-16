62c2ce98ef7b196f0bf2891692f87315e5c660cf
Juan Paucar <juantotish1@hotmail.com>
2016-02-16 17:34:55 -0500

    agregar fondo

--- vistas/banner.php
+++ vistas/banner.php
@@ -1,6 +1,11 @@
+
 <div class="container">
-  <div class="jumbotron">
-    <h1>Bienvenido al sorteo de PEPSI</h1>
-    <p>En la parte de abajo puedes ingresar tokens que hayan salido en tus gaseosas</p>
+    <DIV ALIGN=center>
+            <img align="CENTER" src="libs/media/css/images/sello.png" class="img-responsive" width="400" height="400"/>
+        </div> <br>
+  <div class="jumbotron" align="center" style="background-color: rgba(221, 221, 221, 0)">
+    <h1 style="color: white">Bienvenido al sorteo de Sociedad Deportivo Quito</h1>
+    <p style="color: white">En la parte de abajo puedes ingresar el toke que haya salido en tu entrada</p>
   </div>
+
 </div>
--- vistas/ingreso_token.php
+++ vistas/ingreso_token.php
@@ -1,17 +1,19 @@
+
 <div class="container">
 
   <div class="col-sm-offset-3 col-sm-6">
 
     <form class="form-inline" action="./token_validator.php" method="POST">
-      <label>Ingresa tu token:</label>
+        <label style="color: white; font-size: 18px">Ingresa tu token: </label>
       <div class="form-group">
         <label class="sr-only" for="token">Canjeo del token</label>
         <div class="input-group">
           <input type="text" class="form-control" id="token" name="token" placeholder="">
         </div>
       </div>
-      <button type="submit" class="btn btn-primary">Canjear token</button>
+      <button type="submit" class="btn btn-primary" style="font-size: 18px">Registrar token</button>
     </form>
+    
 
   </div>
 
